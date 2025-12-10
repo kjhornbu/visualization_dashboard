67e37fed21b320de23619bcd72665489e2a23693
James Cook <jamesjcook@users.noreply.github.com>
2025-12-10 15:29:08 -0500

    Adjusted code to use semi-global variables from persistentData
    Added default_files(dict) to persistent data to hold default paths.
    Set value for input to = the default in addition to the place holders.

--- callbacks.py
+++ callbacks.py
@@ -6,7 +6,8 @@ import time
 import os
 from helper_functions import *
 from classes import *
-from persistentData import Indiv_Data, Group_Data, Group_Stats
+#from persistentData import Indiv_Data, Group_Data, Group_Stats
+import persistentData
 
 # All your callbacks have to be between the end of the app layout and the app run call to work properly
 ## OBTAINING DATA CALLBACKS
@@ -17,7 +18,7 @@ def update_input_stats (path):
     if path is not None and os.path.exists(path):
         time.sleep(1)
         print(path)
-        Group_Stats = load_stats(path)
+        persistentData.Group_Stats = load_stats(path)
         print(f'LOADED GROUP STATS {path}')
     return
 
@@ -26,44 +27,50 @@ def update_input_stats (path):
 def update_input_group_data (path):
     if path is not None and os.path.exists(path):
         time.sleep(1)
-        Group_Data = load_data(path,mode='group')
+        persistentData.Group_Data = load_data(path,mode='group')
         print(f'LOADED GROUP DATA {path}')
     return
 
 @callback(Output("loading-indiv_data", "children"),
     Input(component_id='indiv_data_path',component_property='value'))
 def update_input_indiv_data (path):
-    if path is not None and os.path.exists(path): 
+    if path is not None and os.path.exists(path):
         time.sleep(1)
-        Indiv_Data = load_data(path,mode='indiv')
+        persistentData.Indiv_Data = load_data(path,mode='indiv')
         print(f'LOADED INDIV DATA {path}')
     return
 
-@callback(Output(component_id='prompt-container',component_property='children'),Input(component_id='select-mode',component_property='value'),prevent_initial_call=True)
+@callback(
+    Output(component_id='prompt-container',component_property='children'),
+    Input(component_id='select-mode',component_property='value'),prevent_initial_call=True)
 def make_manual_versus_prompt_input(input_mode):
     if input_mode=='Manual':
-        table_type_dropdown=dcc.Dropdown(options={'stats':'Group Statistical Results','group':'Group Data Table','indiv': 'Subject Data Table'},placeholder='Select Main Table for Visualization', id='main_plot_table')
+        table_type_dropdown=dcc.Dropdown(options={'stats':'Group Statistical Results','group':'Group Data Table','indiv': 'Subject Data Table'},
+                                         placeholder='Select Main Table for Visualization', id='main_plot_table')
         return table_type_dropdown
     elif input_mode =='Prompt':
         prompt_input=dcc.Textarea(value=None,placeholder='Input Prompt for Figure Generation, such as "Show me the Top 10 significant FA regions for Age_Class"',id='prompt_input',style={'width': '100%'})
         return prompt_input
     else:
         return None
-    
-@callback(Output(component_id='output-container', component_property='children', allow_duplicate=True),Input(component_id='prompt_input',component_property='value'),prevent_initial_call=True)
+
+@callback(
+    Output(component_id='output-container', component_property='children', allow_duplicate=True),
+    Input(component_id='prompt_input', component_property='value'),
+    prevent_initial_call=True)
 def set_figure_to_output_Prompt(prompt_text):
     message= 'Make Sure All Data Files Are Loaded and Exist At Path Location'
-    
+
     #parse the group text pulling out key aspects of the request
     #push that to the configuration generation
     #apply that to the figure
     #push that to the output container
-    print(Indiv_Data)
-    print(Group_Data)
-    print(Group_Stats)
-    if isinstance(Indiv_Data, DataStructure) and isinstance(Group_Data, DataStructure) and isinstance(Group_Stats, StatsStructure):
+    print(persistentData.Indiv_Data)
+    print(persistentData.Group_Data)
+    print(persistentData.Group_Stats)
+    if isinstance(persistentData.Indiv_Data, DataStructure) and isinstance(persistentData.Group_Data, DataStructure) and isinstance(persistentData.Group_Stats, StatsStructure):
         return full_fig_layout(select_table_4_plotting)
-    else: 
+    else:
         return [html.Div(className='chart-item', children=[html.Div(children=dcc.Input(id="Error_on_loading_Prompt", value=message))])]
     return None
 
@@ -73,9 +80,10 @@ def set_figure_to_output_Prompt(prompt_text):
 def set_figure_to_output_Manual(select_table_4_plotting):
     message= 'Make Sure All Data Files Are Loaded and Exist At Path Location'
     myConfig=generate_config()
-    app.myConfig=myConfig
-    
-    if isinstance(Indiv_Data, DataStructure) and isinstance(Group_Data, DataStructure) and isinstance(Group_Stats, StatsStructure):
+    # cheesy method to hold onto configs, see persistentData for the name of our "global-ish" variables.o
+    Plot_Configurations["myConfig"]=myConfig
+
+    if isinstance(persistentData.Indiv_Data, DataStructure) and isinstance(persistentData.Group_Data, DataStructure) and isinstance(persistentData.Group_Stats, StatsStructure):
         return full_fig_layout(select_table_4_plotting)
-    else: 
+    else:
         return [html.Div(className='chart-item', children=[html.Div(children=dcc.Input(id="Error_on_loading_Manual", value=message))])]
--- persistentData.py
+++ persistentData.py
@@ -1,4 +1,50 @@
+"""
+persistentData
+
+Global-ish data to share across our dash-app.
+Includes default paths.
+I think these will end up being shared between multiple users at the same time, which may be a mistake.
+
+"""
+import os
+
 
 Indiv_Data='We'
 Group_Data='R'
 Group_Stats='Testing'
+
+# James presumes a dict will be a good choice for plot configurations, as we'll name each one as we add it.
+Plot_Configurations={}
+
+
+
+
+# Kathryn defaults
+itab1='/Volumes/dusom_civm-kjh60/All_Staff/18.gaj.42/Scalar_and_Volume/Main_Effects_2025_01_14_NoB6/Non_Erode/Subject_Data_Table.csv'
+gtab1='/Volumes/dusom_civm-kjh60/All_Staff/18.gaj.42/Scalar_and_Volume/Main_Effects_2025_01_14_NoB6/Non_Erode/Bilateral/Group_Data_Table_Age_Class_Strain_Sex.csv'
+rtab1='/Volumes/dusom_civm-kjh60/All_Staff/18.gaj.42/Scalar_and_Volume/Main_Effects_2025_01_14_NoB6/Non_Erode/Bilateral/Group_Statistical_Results_Age_Class_Strain_Sex.csv'
+
+# James defaults
+itab2='/home/james/mnt/dusom_civm_kjh60/All_Staff/18.gaj.42/Scalar_and_Volume/Main_Effects_2025_01_14_NoB6/Non_Erode/Subject_Data_Table.csv'
+gtab2='/home/james/mnt/dusom_civm_kjh60/All_Staff/18.gaj.42/Scalar_and_Volume/Main_Effects_2025_01_14_NoB6/Non_Erode/Bilateral/Group_Data_Table_Age_Class_Strain_Sex.csv'
+rtab2='/home/james/mnt/dusom_civm_kjh60/All_Staff/18.gaj.42/Scalar_and_Volume/Main_Effects_2025_01_14_NoB6/Non_Erode/Bilateral/Group_Statistical_Results_Age_Class_Strain_Sex.csv'
+
+# Other friend derfaults?
+# itab3=...
+# gtab3=...
+# rtab3=...
+
+itab=itab1
+gtab=gtab1
+rtab=rtab1
+if os.path.exists(itab2) and not os.path.exists(itab1):
+    itab=itab2
+if os.path.exists(gtab2) and not os.path.exists(gtab1):
+    gtab=gtab2
+if os.path.exists(rtab2) and not os.path.exists(rtab1):
+    rtab=rtab2
+
+default_files={"Subject": itab,
+             "Group": gtab,
+             "Result": rtab,
+             }
